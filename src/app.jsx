bc381650614b5366815df8e80956da228b070ea8
mohammed <zchizzz11111@gmail.com>
2024-04-25 15:16:23 +0200

    frist commit

---
+++ src/app.jsx
@@ -0,0 +1,40 @@
+
+import styles from "./style";
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+import './index.css'
+
+export function App() {
+
+  const year = new Date
+
+  return (
+    <>
+      <div className="bg-primary w-full overflow-hidden">
+          <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+              <Navbar />
+            </div>
+          </div>
+
+          <div className={`bg-primary ${styles.flexStart}`}>
+            <div className={`${styles.boxWidth}`}>
+              <Hero />
+            </div>
+          </div>
+          
+          <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+              <Stats />
+              <Business />
+              <Billing />
+              <CardDeal />
+              <Testimonials />
+              <Clients />
+              <CTA />
+              <Footer />
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
